0a18cbfdb2d2fd48b7920ce334741be4bee8f7e7
KazzyJr <cazacu.serjio@live.com>
2023-01-31 00:58:22 +0200

    Added setup.py V1.1.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='MPC-HC-MultiStart',
+    version='1.1.1',
+    packages=['build'],
+    license='MIT',
+    author='Cazacu Sergiu',
+    author_email='cazacu.serjio@gmail.com',
+    description='A project that handles multiple synchronized instances of MPC-HC.'
+)
