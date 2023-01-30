a418f309752c737931d65ec6622ed4773812fd3d
KazzyJr <cazacu.serjio@live.com>
2023-01-30 23:04:44 +0200

    Time argument Revision 3 V1.0.3

--- Multi_Run_MPC.py
+++ Multi_Run_MPC.py
@@ -1,67 +1,125 @@
 """
 Original code by: KazzyJr (c)2021
-Latest revision: 06.09.2021
+Latest revision: 30.01.2023
 """
+import json
 import os
-import sys
+import pathlib
+
+SECONDS_IN_HOUR = 3600
+SECONDS_IN_MINUTE = 60
+CONFIGURATION = {}
+DEFAULT_START_TIME = "00:00:00"
 
 
 class Time:
-	def __init__(self, given_time):
+	"""Handles time differential calculation"""
+	def __init__(self, given_time: str):
+		"""
+		Sets an internal reference time.
+		:param given_time: A string representing the given start time in HH:MM:SS format
+		"""
 		hour, minute, seconds = given_time.split(":")
 		self.hour = int(hour)
 		self.minute = int(minute)
 		self.seconds = int(seconds)
 		self.list = [self.hour, self.minute, self.seconds]
 
-	string_condition = 0
+	string_condition: bool = False
 
-	def calculate_time_span(self, time_difference) -> str:
-		total_seconds = self.hour * 3600 + self.minute * 60 + self.seconds
+	def calculate_time_span(self, time_difference: int) -> str:
+		"""
+		Calculates the time span using the time_difference. If the resulting time is out of bounds, defaults to
+		the original time.
+		"""
+		total_seconds = self.hour * SECONDS_IN_HOUR + self.minute * SECONDS_IN_MINUTE + self.seconds
 		target_time = total_seconds + time_difference
-		self.hour = target_time//3600
-		self.minute = (target_time - self.hour * 3600)//60
-		self.seconds = target_time - self.hour * 3600 - self.minute * 60
+		self.hour = target_time//SECONDS_IN_HOUR
+		self.minute = (target_time - self.hour * SECONDS_IN_HOUR)//SECONDS_IN_MINUTE
+		self.seconds = target_time - self.hour * SECONDS_IN_HOUR - self.minute * SECONDS_IN_MINUTE
 		self.list = [self.hour, self.minute, self.seconds]
 		if target_time < 0:
 			raise ValueError("Negative time")
-		for item in self.list:
+		for index, item in enumerate(self.list):
 			if 0 <= item < 10:
-				temp_index = self.list.index(item)
-				item = "0" + str(item)
-				self.list[temp_index] = item
-				self.string_condition = 1
-		if self.string_condition == 1:
-			self.string_condition = 0
+				item = f"0{str(item)}"
+				self.list[index] = item
+				self.string_condition = True
+		if self.string_condition:
+			self.string_condition = False
 			return f"{self.list[0]}:{self.list[1]}:{self.list[2]}"
 		else:
 			return f"{self.hour}:{self.minute}:{self.seconds}"
 
 
-def main():
+class Movie:
+	"""Handles movie objects."""
+	all_movies = []
 
-	try:
-		time = sys.argv[1]
-	except IndexError:
-		time = 0
+	def __init__(self, file_path: str, start_time: str = DEFAULT_START_TIME):
+		"""
+		Initializer for the movies. Sets all the values and remembers the instances.
 
-	for i in files_sync_time.values():
-		end_time = Time(i).calculate_time_span(int(time))
-		keys = list(files_sync_time.keys())
-		values = list(files_sync_time.values())
-		cmd_complete = base_command + " \"" + keys[values.index(i)] + "\"" + argument1 + end_time + argument2
-		os.system(f'cmd /c "{cmd_complete}"')
+		:param file_path: A path to the movie to be opened
+		:param start_time: A string representing the initial start time in HH:MM:SS format
+		"""
+		self.start_time = start_time
+		self.file_path = file_path
+		self.new_start_time = ''
+		Movie.all_movies.append(self)
+
+	def __str__(self):
+		return f'Movie located at: \n{self.file_path}\nhas an initial start time at {self.start_time} and an offset ' \
+		       f'start time at {self.new_start_time}'
+
+
+def load_configuration():
+	"""Global configuration object setting"""
+	global CONFIGURATION
+	config_text = pathlib.Path('config.json').read_text(encoding='utf-8')
+	CONFIGURATION = json.loads(config_text)
+
+
+def load_movies(json_path: pathlib.Path):
+	"""Loads movies to the Movie class, keeping track of the instances"""
+	movies_txt = json_path.read_text(encoding='utf-8')
+	data = json.loads(movies_txt)
+	for path, start_time in data.items():
+		Movie(file_path=path, start_time=start_time)
+
+
+def build_commands() -> list[str]:
+	"""Calculates the time differentials and creates the command list"""
+	all_commands = []
+	for movie in Movie.all_movies:
+		new_start_time = Time(movie.start_time).calculate_time_span(int(CONFIGURATION.get('time_offset')))
+		movie.new_start_time = new_start_time
+		cmd_complete = "{} \"{}\"{}{}{}".format(
+				CONFIGURATION.get('base_command'),
+				movie.file_path,
+				CONFIGURATION.get('first_argument'),
+				new_start_time,
+				CONFIGURATION.get('second_argument')
+		)
+		all_commands.append(cmd_complete)
+		if not CONFIGURATION.get("quiet"):
+			print(movie)
+	return all_commands
+
+
+def start_execution(command_list: list[str]):
+	"""Launches the MPC-HC processes"""
+	if CONFIGURATION.get("dry_run"):
+		for cmd in command_list:
+			print(f'Would execute: cmd /c "{cmd}"')
+	else:
+		for cmd in command_list:
+			os.system(f'cmd /c "{cmd}"')
 
 
 if __name__ == "__main__":
-	# Populated with a direct path to a video and a target time from which we will add or subtract time
-	files_sync_time = {
-		"D:\\Videos\\Testing Video\\Placeholder.mp4": "00:15:00",
-		"D:\\Videos\\Testing Video\\Placeholders.mp4": "00:15:00"
-	}
-	# MPC path
-	base_command = "START \"C:\\Program Files (x86)\\K-Lite Codec Pack\\MPC-HC64\\\" mpc-hc64.exe"
-	argument1 = " /startpos "
-	argument2 = " /new"
-	main()
+	load_configuration()
+	load_movies(pathlib.Path(CONFIGURATION.get('path_to_movies_json')))
+	commands = build_commands()
+	start_execution(commands)
 
--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # MPC-MultiStart
-Start Offset Videos in tandem
+`Start Offset Videos in tandem`
 
--> A tool that allows you to start synchronized video players with or without offset.
+A tool that allows you to start synchronized MPC-HC video players with or without offset.
 
-The argument given upon running the script is just an integer that represents the nr.
-of seconds to get on forward or backward from the given dictionary time of the files.
+### The Config
 
-Modify the files_sync_time to add your paths and start times.
-Modify the base commad path to suit your media player classic (required) installation location.
+The `config.json` object is used for adjusting the run parameters of the tool.
+* `base_command` refers to the base command that is used to spawn an MPC-HC instance;
+* `first_argument` refers to the start position indicator;
+* `second_argument` refers to the MPC-HC target instance toggle;
+* `path_to_movies_json` refers to the JSON object which contains the file-start_time mapping;
+* `time_offset` refers to the time added (positive integer) or subtracted (negative integer) from all the times of the 
+  yet to be launched movies;
+* `quiet` refers to whether the application prints or not;
+* `dry_run` refers to the application only printing the end commands;
+
+### The input JSON
+
+The `movies_template.json` object is used for specifying a path to time mapping.
+```json
+{
+  "path/to/file1.mp4": "00:01:15",
+  "path/to/file2.mp4": "00:02:15",
+  "path/to/file3.mp4": "00:03:30"
+}
+```
+The files in the mapping will be started simultaneously at the times indicated (if the configured offset is 0), or 
+using the calculated offset (if the configured offset is non-0)
